38801957dc0dfce64cb591f99e8afc1c65bc957d
秦天翔 <rushi_wowen@163.com>
2026-04-04 15:55:17 +0800

    use HomeScreenV8 usable version

---
+++ xiaolongxia_voice_assistant/flutter_app/lib/main_release_v6.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen_v8.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const XiaoLongXiaApp());
+}
+
+class XiaoLongXiaApp extends StatelessWidget {
+  const XiaoLongXiaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const HomeScreenV8(),
+    );
+  }
+}
